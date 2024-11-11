cad066de7d0a33e24c2f00cef7fd1b578157d81a
russel6879 <dev103@cems.com.bd>
2024-11-11 18:12:07 +0600

    meta dynamic

--- services/settingsService.js
+++ services/settingsService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // Replace with your API base URL
+  baseURL: 'http://expogazette.com/api', // Replace with your API base URL
   withCredentials: false,
   headers: {
     Accept: 'application/json',
